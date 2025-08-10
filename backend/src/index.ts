1efe3bce61876b1df0625cfe0e6dca092244e3c6
Arshpreet62 <arshpreet6462@gmail.com>
2025-08-10 22:56:25 +0530

    fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,25 +11,13 @@ import { authenticateJWT } from "./authMiddleware";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://postman-clone-git-main-arshpreet62s-projects.vercel.app",
-];
-
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: ["https://postman-clone-git-main-arshpreet62s-projects.vercel.app"],
     credentials: true,
   })
 );
 
-app.options("*", cors());
 app.use(express.json());
 
 mongoose
